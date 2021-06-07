c8c637a3b16cc1d70ce82d4928a5d2efd8c625ae
ethanlaj <ethan@lajfam.com>
2021-06-06 23:44:48 -0400

    delete lame categories

--- categories.js
+++ categories.js
@@ -1,31 +1,31 @@
 /* eslint-disable line-comment-position */
 
 module.exports.CATEGORIES = [
-	{ category: '20081', channel: '849270047125405716'}, // Antiques
-	{ category: '550', channel: '849270251563778098'}, // Art
+	//{ category: '20081', channel: '849270047125405716'}, // Antiques
+	//{ category: '550', channel: '849270251563778098'}, // Art
 	{ category: '2984', channel: '849270638622277653'}, // Baby
-	{ category: '267', channel: '849270850465169478'}, // Books/Magazines
+	//{ category: '267', channel: '849270850465169478'}, // Books/Magazines
 	{ category: '12576', channel: '848914693106958386' }, // Business/Industrial
 	{ category: '625', channel: '849271708950593546'}, // Cameras/Photo
 	{ category: '15032', channel: '849272064400556073'}, // Cell Phones/Accessories
-	{ category: '11450', channel: '848914787592831046' }, // Clothing/Shoes/Accessories
+	//{ category: '11450', channel: '848914787592831046' }, // Clothing/Shoes/Accessories
 	{ category: '1', channel: '848914861383090186' }, // Collectibles
 	{ category: '58058', channel: '848915570635046985' }, // Computers/Tablets/Networking
 	{ category: '293', channel: '848914926314586142' }, // Electronics
 	{ category: '14339', channel: '848915015548665856' }, // Crafts
 	{ category: '237', channel: '848915084692160553' }, // Dolls/Bears
 	{ category: '11232', channel: '849274354935398421'}, // Movies/TV
-	{ category: '45100', channel: '849274712965120095'}, // Entertainment Memorabilia
+	//{ category: '45100', channel: '849274712965120095'}, // Entertainment Memorabilia
 	{ category: '26395', channel: '849267384618319882'}, // Health / Beauty
 	{ category: '11700', channel: '848915241856401448' }, // Home/Garden
-	{ category: '281', channel: '849275393148190770'}, // Jewelry & Watches
+	//{ category: '281', channel: '849275393148190770'}, // Jewelry & Watches
 	{ category: '11233', channel: '849275645846355968'}, // Music
 	{ category: '619', channel: '849275728776396880'}, // Musical Instruments Gear
 	{ category: '1281', channel: '848915322504609812' }, // Pet Supplies
 	{ category: '870', channel: '849276218935083029'}, // Pottery / Glass
 	{ category: '888', channel: '848915349997879296' }, // Sporting Goods
 	{ category: '220', channel: '848915411110068235' }, // Toys/Hobbies
-	{ category: '3252', channel: '849276937793044532'}, // Travel
+	//{ category: '3252', channel: '849276937793044532'}, // Travel
 	{ category: '1249', channel: '849277292290768896'}, // Video Games & Consoles
-	{ category: '6000', channel: '849266435192782848'}, // Automotive
+	//{ category: '6000', channel: '849266435192782848'}, // Automotive
 ];
