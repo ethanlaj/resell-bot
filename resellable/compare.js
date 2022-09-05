37d20ecd1bb7f8e945130d3b22c108d156569207
Ethan Lajeunesse <ethan@lajfam.com>
2022-09-04 20:30:02 -0400

    Change variable name, change package name

--- resellable/compare.js
+++ resellable/compare.js
@@ -1,11 +1,11 @@
 const COMPARE_PRICE = 35;
 const MULTIPLIER = 1.5;
-const minimumWalmartPrice = 15;
+const MIN_WALMART_PRICE = 15;
 
 /*For faster debugging
 const COMPARE_PRICE = 0;
 const MULTIPLIER = 0.1;
-const minimumWalmartPrice = 0;*/
+const MIN_WALMART_PRICE = 0;*/
 
 let upcCache = require("./upcCache.js").array;
 
@@ -13,7 +13,7 @@ module.exports.run = function compare(walmart, ebay) {
 	let ebayPrice = Number(ebay.sellingStatus[0].currentPrice[0].__value__);
 	let walmartPrice = walmart.salePrice;
 
-	if (walmartPrice > minimumWalmartPrice &&
+	if (walmartPrice > MIN_WALMART_PRICE &&
 		(ebayPrice - COMPARE_PRICE > walmartPrice || walmartPrice * MULTIPLIER < ebayPrice) &&
 		!walmart.marketplace &&
 		(!walmart.offerType || walmart.offerType === "ONLINE_AND_STORE") &&
